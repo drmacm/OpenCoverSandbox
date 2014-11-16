7f87ef65be8da25e2e0f9926c547701a00798455
drmacm <drmac.mladen@gmail.com>
2014-11-16 13:06:59 +0100

    Commenting out just attributes

--- IBll/ITestBll.cs
+++ IBll/ITestBll.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+using System.Diagnostics.Contracts;
 namespace IBll
 {
     //[ContractClass(typeof(ITestBllContract))]
@@ -9,13 +11,13 @@ namespace IBll
 
 
     //[ContractClassFor(typeof(ITestBll))]
-    //internal abstract class ITestBllContract : ITestBll
-    //{
-    //    public int Method1(int id)
-    //    {
-    //        Contract.Requires<ArgumentException>(id > 0, "Value must be greater than zero");
+    internal abstract class ITestBllContract : ITestBll
+    {
+        public int Method1(int id)
+        {
+            Contract.Requires<ArgumentException>(id > 0, "Value must be greater than zero");
 
-    //        return default(int);
-    //    }
-    //}
+            return default(int);
+        }
+    }
 }
